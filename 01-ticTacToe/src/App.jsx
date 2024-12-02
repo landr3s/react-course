c87f36be63c25ec34a6996f1b7afc6d48bd3ffe7
landr3s <laskindr3s@gmail.com>
2024-12-02 12:17:02 -0500

    Tic Tac Toe Modal

--- 01-ticTacToe/src/App.jsx
+++ 01-ticTacToe/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { TURNS } from './consts'
+import { TURNS, WINNER_COMBOS } from './consts'
 import { Square } from './components/Square'
 import './App.css'
 
@@ -8,12 +8,30 @@ export default function App() {
   const [turn, setTurn] = useState(TURNS.X)
   const [winner, setWinner] = useState(null)
 
+  const checkWinner = boardToCheck => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
   const updateBoard = index => {
+    if (board[index] || winner) return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   const resetBoard = () => {
@@ -43,6 +61,13 @@ export default function App() {
         {<Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>}
         {<Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>}
       </section>
+      {winner ? (
+        <section className='board-modal'>
+          <button>Play Again</button>
+        </section>
+      ) : (
+        ''
+      )}
     </>
   )
 }
